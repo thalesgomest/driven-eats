6ff5484105c57dbad17571b60369bfbddf18f565
Thales Gomes Targino <thalestargino@gmail.com>
2022-01-26 01:40:37 -0300

    projeto3-driveneats v1.2 - Select Dishes, Drinks and Desserts

---
+++ scripts.js
@@ -0,0 +1,279 @@
+
+// * Functions select Dishes
+
+function Select_frango_yin_yang() {
+    document.getElementById("box_frango_yin_yang").style.borderColor = "#32B72F";
+    document.getElementById("check_frango_yin_yang").style.display = "block";
+    document.getElementById("box_lasanha_bolonhesa").style.borderColor = "#FFFFFF";
+    document.getElementById("check_lasanha_bolonhesa").style.display = "none";
+    document.getElementById("box_parmegiana_carne").style.borderColor = "#FFFFFF";
+    document.getElementById("check_parmegiana_carne").style.display = "none";
+    document.getElementById("box_parmegiana_frango").style.borderColor = "#FFFFFF";
+    document.getElementById("check_parmegiana_frango").style.display = "none";
+    document.getElementById("box_bife_a_cavalo").style.borderColor = "#FFFFFF";
+    document.getElementById("check_bife_a_cavalo").style.display = "none";
+    document.getElementById("box_bife_com_fritas").style.borderColor = "#FFFFFF";
+    document.getElementById("check_bife_com_fritas").style.display = "none";
+}
+
+function Select_lasanha_bolonhesa() {
+    document.getElementById("box_frango_yin_yang").style.borderColor = "#FFFFFF";
+    document.getElementById("check_frango_yin_yang").style.display = "none";
+    document.getElementById("box_lasanha_bolonhesa").style.borderColor = "#32B72F";
+    document.getElementById("check_lasanha_bolonhesa").style.display = "block";
+    document.getElementById("box_parmegiana_carne").style.borderColor = "#FFFFFF";
+    document.getElementById("check_parmegiana_carne").style.display = "none";
+    document.getElementById("box_parmegiana_frango").style.borderColor = "#FFFFFF";
+    document.getElementById("check_parmegiana_frango").style.display = "none";
+    document.getElementById("box_bife_a_cavalo").style.borderColor = "#FFFFFF";
+    document.getElementById("check_bife_a_cavalo").style.display = "none";
+    document.getElementById("box_bife_com_fritas").style.borderColor = "#FFFFFF";
+    document.getElementById("check_bife_com_fritas").style.display = "none";
+}
+
+function Select_parmegiana_carne() {
+    document.getElementById("box_frango_yin_yang").style.borderColor = "#FFFFFF";
+    document.getElementById("check_frango_yin_yang").style.display = "none";
+    document.getElementById("box_lasanha_bolonhesa").style.borderColor = "#FFFFFF";
+    document.getElementById("check_lasanha_bolonhesa").style.display = "none";
+    document.getElementById("box_parmegiana_carne").style.borderColor = "#32B72F";
+    document.getElementById("check_parmegiana_carne").style.display = "block";
+    document.getElementById("box_parmegiana_frango").style.borderColor = "#FFFFFF";
+    document.getElementById("check_parmegiana_frango").style.display = "none";
+    document.getElementById("box_bife_a_cavalo").style.borderColor = "#FFFFFF";
+    document.getElementById("check_bife_a_cavalo").style.display = "none";
+    document.getElementById("box_bife_com_fritas").style.borderColor = "#FFFFFF";
+    document.getElementById("check_bife_com_fritas").style.display = "none";
+}
+
+function Select_parmegiana_frango() {
+    document.getElementById("box_frango_yin_yang").style.borderColor = "#FFFFFF";
+    document.getElementById("check_frango_yin_yang").style.display = "none";
+    document.getElementById("box_lasanha_bolonhesa").style.borderColor = "#FFFFFF";
+    document.getElementById("check_lasanha_bolonhesa").style.display = "none";
+    document.getElementById("box_parmegiana_carne").style.borderColor ="#FFFFFF";
+    document.getElementById("check_parmegiana_carne").style.display = "none";
+    document.getElementById("box_parmegiana_frango").style.borderColor = "#32B72F";
+    document.getElementById("check_parmegiana_frango").style.display = "block";
+    document.getElementById("box_bife_a_cavalo").style.borderColor = "#FFFFFF";
+    document.getElementById("check_bife_a_cavalo").style.display = "none";
+    document.getElementById("box_bife_com_fritas").style.borderColor = "#FFFFFF";
+    document.getElementById("check_bife_com_fritas").style.display = "none";
+
+}
+
+function Select_bife_a_cavalo() {
+    document.getElementById("box_frango_yin_yang").style.borderColor = "#FFFFFF";
+    document.getElementById("check_frango_yin_yang").style.display = "none";
+    document.getElementById("box_lasanha_bolonhesa").style.borderColor = "#FFFFFF";
+    document.getElementById("check_lasanha_bolonhesa").style.display = "none";
+    document.getElementById("box_parmegiana_carne").style.borderColor ="#FFFFFF";
+    document.getElementById("check_parmegiana_carne").style.display = "none";
+    document.getElementById("box_parmegiana_frango").style.borderColor = "#FFFFFF";
+    document.getElementById("check_parmegiana_frango").style.display = "none";
+    document.getElementById("box_bife_a_cavalo").style.borderColor = "#32B72F";
+    document.getElementById("check_bife_a_cavalo").style.display = "block";
+    document.getElementById("box_bife_com_fritas").style.borderColor = "#FFFFFF";
+    document.getElementById("check_bife_com_fritas").style.display = "none";
+}
+
+function Select_bife_com_fritas() {
+    document.getElementById("box_frango_yin_yang").style.borderColor = "#FFFFFF";
+    document.getElementById("check_frango_yin_yang").style.display = "none";
+    document.getElementById("box_lasanha_bolonhesa").style.borderColor = "#FFFFFF";
+    document.getElementById("check_lasanha_bolonhesa").style.display = "none";
+    document.getElementById("box_parmegiana_carne").style.borderColor ="#FFFFFF";
+    document.getElementById("check_parmegiana_carne").style.display = "none";
+    document.getElementById("box_parmegiana_frango").style.borderColor = "#FFFFFF";
+    document.getElementById("check_parmegiana_frango").style.display = "none";
+    document.getElementById("box_bife_a_cavalo").style.borderColor = "#FFFFFF";
+    document.getElementById("check_bife_a_cavalo").style.display = "none";
+    document.getElementById("box_bife_com_fritas").style.borderColor = "#32B72F";
+    document.getElementById("check_bife_com_fritas").style.display = "block";
+}
+
+// * Functions select Drinks
+
+function Select_coca_cola_refri() {
+    document.getElementById("box_coca_cola_refri").style.borderColor = "#32B72F";
+    document.getElementById("check_coca_cola_refri").style.display = "block";
+    document.getElementById("box_guarana_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_guarana_refri").style.display = "none";
+    document.getElementById("box_guarana_jesus_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_guarana_jesus_refri").style.display = "none";
+    document.getElementById("box_pepsi_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_pepsi_refri").style.display = "none";
+    document.getElementById("box_sprite_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_sprite_refri").style.display = "none";
+    document.getElementById("box_fanta_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_fanta_refri").style.display = "none";
+}
+
+function Select_guarana_refri() {
+    document.getElementById("box_coca_cola_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_coca_cola_refri").style.display = "none";
+    document.getElementById("box_guarana_refri").style.borderColor = "#32B72F";
+    document.getElementById("check_guarana_refri").style.display = "block";
+    document.getElementById("box_guarana_jesus_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_guarana_jesus_refri").style.display = "none";
+    document.getElementById("box_pepsi_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_pepsi_refri").style.display = "none";
+    document.getElementById("box_sprite_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_sprite_refri").style.display = "none";
+    document.getElementById("box_fanta_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_fanta_refri").style.display = "none";
+}
+
+function Select_guarana_jesus_refri() {
+    document.getElementById("box_coca_cola_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_coca_cola_refri").style.display = "none";
+    document.getElementById("box_guarana_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_guarana_refri").style.display = "none";
+    document.getElementById("box_guarana_jesus_refri").style.borderColor = "#32B72F";
+    document.getElementById("check_guarana_jesus_refri").style.display = "block";
+    document.getElementById("box_pepsi_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_pepsi_refri").style.display = "none";
+    document.getElementById("box_sprite_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_sprite_refri").style.display = "none";
+    document.getElementById("box_fanta_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_fanta_refri").style.display = "none";
+}
+
+function Select_pepsi_refri() {
+    document.getElementById("box_coca_cola_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_coca_cola_refri").style.display = "none";
+    document.getElementById("box_guarana_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_guarana_refri").style.display = "none";
+    document.getElementById("box_guarana_jesus_refri").style.borderColor ="#FFFFFF";
+    document.getElementById("check_guarana_jesus_refri").style.display = "none";
+    document.getElementById("box_pepsi_refri").style.borderColor = "#32B72F";
+    document.getElementById("check_pepsi_refri").style.display = "block";
+    document.getElementById("box_sprite_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_sprite_refri").style.display = "none";
+    document.getElementById("box_fanta_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_fanta_refri").style.display = "none";
+
+}
+
+function Select_sprite_refri() {
+    document.getElementById("box_coca_cola_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_coca_cola_refri").style.display = "none";
+    document.getElementById("box_guarana_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_guarana_refri").style.display = "none";
+    document.getElementById("box_guarana_jesus_refri").style.borderColor ="#FFFFFF";
+    document.getElementById("check_guarana_jesus_refri").style.display = "none";
+    document.getElementById("box_pepsi_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_pepsi_refri").style.display = "none";
+    document.getElementById("box_sprite_refri").style.borderColor = "#32B72F";
+    document.getElementById("check_sprite_refri").style.display = "block";
+    document.getElementById("box_fanta_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_fanta_refri").style.display = "none";
+}
+
+function Select_fanta_refri() {
+    document.getElementById("box_coca_cola_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_coca_cola_refri").style.display = "none";
+    document.getElementById("box_guarana_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_guarana_refri").style.display = "none";
+    document.getElementById("box_guarana_jesus_refri").style.borderColor ="#FFFFFF";
+    document.getElementById("check_guarana_jesus_refri").style.display = "none";
+    document.getElementById("box_pepsi_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_pepsi_refri").style.display = "none";
+    document.getElementById("box_sprite_refri").style.borderColor = "#FFFFFF";
+    document.getElementById("check_sprite_refri").style.display = "none";
+    document.getElementById("box_fanta_refri").style.borderColor = "#32B72F";
+    document.getElementById("check_fanta_refri").style.display = "block";
+}
+
+// * Functions select Desserts
+
+function Select_pudim() {
+    document.getElementById("box_pudim").style.borderColor = "#32B72F";
+    document.getElementById("check_pudim").style.display = "block";
+    document.getElementById("box_mousse_chocolate").style.borderColor = "#FFFFFF";
+    document.getElementById("check_mousse_chocolate").style.display = "none";
+    document.getElementById("box_mousse_maracuja").style.borderColor = "#FFFFFF";
+    document.getElementById("check_mousse_maracuja").style.display = "none";
+    document.getElementById("box_torta_holandesa").style.borderColor = "#FFFFFF";
+    document.getElementById("check_torta_holandesa").style.display = "none";
+    document.getElementById("box_torta_limao").style.borderColor = "#FFFFFF";
+    document.getElementById("check_torta_limao").style.display = "none";
+    document.getElementById("box_paçoca").style.borderColor = "#FFFFFF";
+    document.getElementById("check_paçoca").style.display = "none";
+}
+
+function Select_mousse_chocolate() {
+    document.getElementById("box_pudim").style.borderColor = "#FFFFFF";
+    document.getElementById("check_pudim").style.display = "none";
+    document.getElementById("box_mousse_chocolate").style.borderColor = "#32B72F";
+    document.getElementById("check_mousse_chocolate").style.display = "block";
+    document.getElementById("box_mousse_maracuja").style.borderColor = "#FFFFFF";
+    document.getElementById("check_mousse_maracuja").style.display = "none";
+    document.getElementById("box_torta_holandesa").style.borderColor = "#FFFFFF";
+    document.getElementById("check_torta_holandesa").style.display = "none";
+    document.getElementById("box_torta_limao").style.borderColor = "#FFFFFF";
+    document.getElementById("check_torta_limao").style.display = "none";
+    document.getElementById("box_paçoca").style.borderColor = "#FFFFFF";
+    document.getElementById("check_paçoca").style.display = "none";
+}
+
+function Select_mousse_maracuja() {
+    document.getElementById("box_pudim").style.borderColor = "#FFFFFF";
+    document.getElementById("check_pudim").style.display = "none";
+    document.getElementById("box_mousse_chocolate").style.borderColor = "#FFFFFF";
+    document.getElementById("check_mousse_chocolate").style.display = "none";
+    document.getElementById("box_mousse_maracuja").style.borderColor = "#32B72F";
+    document.getElementById("check_mousse_maracuja").style.display = "block";
+    document.getElementById("box_torta_holandesa").style.borderColor = "#FFFFFF";
+    document.getElementById("check_torta_holandesa").style.display = "none";
+    document.getElementById("box_torta_limao").style.borderColor = "#FFFFFF";
+    document.getElementById("check_torta_limao").style.display = "none";
+    document.getElementById("box_paçoca").style.borderColor = "#FFFFFF";
+    document.getElementById("check_paçoca").style.display = "none";
+}
+
+function Select_torta_holandesa() {
+    document.getElementById("box_pudim").style.borderColor = "#FFFFFF";
+    document.getElementById("check_pudim").style.display = "none";
+    document.getElementById("box_mousse_chocolate").style.borderColor = "#FFFFFF";
+    document.getElementById("check_mousse_chocolate").style.display = "none";
+    document.getElementById("box_mousse_maracuja").style.borderColor ="#FFFFFF";
+    document.getElementById("check_mousse_maracuja").style.display = "none";
+    document.getElementById("box_torta_holandesa").style.borderColor = "#32B72F";
+    document.getElementById("check_torta_holandesa").style.display = "block";
+    document.getElementById("box_torta_limao").style.borderColor = "#FFFFFF";
+    document.getElementById("check_torta_limao").style.display = "none";
+    document.getElementById("box_paçoca").style.borderColor = "#FFFFFF";
+    document.getElementById("check_paçoca").style.display = "none";
+
+}
+
+function Select_torta_limao() {
+    document.getElementById("box_pudim").style.borderColor = "#FFFFFF";
+    document.getElementById("check_pudim").style.display = "none";
+    document.getElementById("box_mousse_chocolate").style.borderColor = "#FFFFFF";
+    document.getElementById("check_mousse_chocolate").style.display = "none";
+    document.getElementById("box_mousse_maracuja").style.borderColor ="#FFFFFF";
+    document.getElementById("check_mousse_maracuja").style.display = "none";
+    document.getElementById("box_torta_holandesa").style.borderColor = "#FFFFFF";
+    document.getElementById("check_torta_holandesa").style.display = "none";
+    document.getElementById("box_torta_limao").style.borderColor = "#32B72F";
+    document.getElementById("check_torta_limao").style.display = "block";
+    document.getElementById("box_paçoca").style.borderColor = "#FFFFFF";
+    document.getElementById("check_paçoca").style.display = "none";
+}
+
+function Select_paçoca() {
+    document.getElementById("box_pudim").style.borderColor = "#FFFFFF";
+    document.getElementById("check_pudim").style.display = "none";
+    document.getElementById("box_mousse_chocolate").style.borderColor = "#FFFFFF";
+    document.getElementById("check_mousse_chocolate").style.display = "none";
+    document.getElementById("box_mousse_maracuja").style.borderColor ="#FFFFFF";
+    document.getElementById("check_mousse_maracuja").style.display = "none";
+    document.getElementById("box_torta_holandesa").style.borderColor = "#FFFFFF";
+    document.getElementById("check_torta_holandesa").style.display = "none";
+    document.getElementById("box_torta_limao").style.borderColor = "#FFFFFF";
+    document.getElementById("check_torta_limao").style.display = "none";
+    document.getElementById("box_paçoca").style.borderColor = "#32B72F";
+    document.getElementById("check_paçoca").style.display = "block";
+}
